1004b1ab70fbd988cc029ca4d7b13e6760f2f4d9
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2024-04-22 17:23:09 -0400

    Update Policy.java
    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -9,33 +9,12 @@ public class Policy{
 
    private String policyNumber;
    private String providerName;
-   private String holderFirstName;
-   private String holderLastName;
-   private int holderAge;
-   private double holderHeight;
-   private double holderWeight;
-   private String smokingStatus;
+   private PolicyHolder policyHolder;
    
-   public Policy(){
-      policyNumber = "";
-      providerName = "";
-      holderFirstName = "";
-      holderLastName = "";
-      holderAge = 0;
-      holderHeight = 0.0;
-      holderWeight = 0.0;
-      smokingStatus = "";
-   }
-   
-   public Policy(String policyNumber, String providerName, String holderFirstName, String holderLastName, int holderAge, double holderHeight, double holderWeight, String smokingStatus){
+   public Policy(String policyNumber, String providerName, PolicyHolder policyHolder){
       this.policyNumber = policyNumber;
       this.providerName = providerName;
-      this.holderFirstName = holderFirstName;
-      this.holderLastName = holderLastName;
-      this.holderAge = holderAge;
-      this.holderHeight = holderHeight;
-      this.holderWeight = holderWeight;
-      this.smokingStatus = smokingStatus;
+      this.policyHolder = policyHolder;
    }
    
    public String getPolicyNumber(){
@@ -53,72 +32,25 @@ public class Policy{
    public void setProviderName(String providerName){
       this.providerName = providerName;
    }
-   
-   public String getHolderFirstName(){
-      return holderFirstName;
+      
+   public void setPolicyHolder(){
+      this.policyHolder = policyHolder;
    }
    
-   public void setHolderFirstName(String holderFirstName){
-     this.holderFirstName = holderFirstName;
-   }
-   
-   public String getHolderLastName(){
-      return holderLastName;
-   }
-   
-   public void setHolderLastName(String holderLastName){
-      this.holderLastName = holderLastName;
-   }
-   
-   public int getHolderAge(){
-      return holderAge;
-   }
-   
-   public void setAge(int holderAge){
-      this.holderAge = holderAge;
-   } 
-   
-   public double getHolderHeight(){
-      return holderHeight;
-   }
-   
-   public void setHeight(double holderHeight){
-      this.holderHeight = holderHeight;
-   }
-   
-   public double getHolderWeight(){
-      return holderWeight;
-   }
-   
-   public void setWeight(double holderWeight){
-      this.holderWeight = holderWeight;
-   }
-   
-   public String getSmokingStatus(){
-      return smokingStatus;
-   }
-   
-   public void setSmokingStatus(String smokingStatus){
-      this.smokingStatus = smokingStatus;
-   }
-   
-   public double calculateBMI(){
-      return (holderWeight * 703) / (holderHeight * holderHeight);
-   }
-   
+   //This method calculate the price of the policy for the policy holder, increasing depending on given information.
    public double calculateInsurancePrice(){
       double baseFee = 600.0;
       double additionalFee = 0.0;
       
-      if(holderAge > 50){
+      if(policyHolder.getHolderAge() > 50){
          additionalFee += 75.0;
       }
       
-      if(smokingStatus.equals("smoker")){
+      if(policyHolder.getSmokingStatus().equals("smoker")){
          additionalFee += 100.0;
       }
       
-      double BMI = calculateBMI();
+      double BMI = policyHolder.calculateBMI();
       if (BMI > 35){
          additionalFee += (BMI - 35) * 20.0;
       }
